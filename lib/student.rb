1f32e4fb5c21e7d21c8bcc6842428ae29ae3892a
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-25 06:38:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
+  attr_accessor :name, :grade
   def initialize(id=nil, name, grade)
     @name = name
     @grade = grade
