7aca42adcb7041dfe0b9beb90393584367c7d782
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-25 06:50:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -27,11 +27,6 @@ class Student
     DB[:conn].execute("DROP TABLE IF EXISTS students;")
   end
 
-  def update
-    sql = "UPDATE students SET name = ?, grade = ? WHERE id =?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end
-
   def save
     if self.id
       self.update
