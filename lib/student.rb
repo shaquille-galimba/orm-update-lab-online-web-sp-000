1badc7e458d5e1c1892d3cfd05b5cdd47823e7f8
Shaquille Galimba <shaqrivergalimba24@gmail.com>
2020-03-25 06:52:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -45,4 +45,10 @@ class Student
       @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
     end
   end
+
+  def self.create(name:, grade:)
+    student = self.new(name, grade)
+    student.save
+    student
+  end
 end
